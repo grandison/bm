0c09e9677b0e112baec0336e83d7097fbc8f5876
Alexander Osmanov <grandison@mail.ru>
2014-02-19 18:12:13 +0400

    Fix

--- app/models/vk_account.rb
+++ app/models/vk_account.rb
@@ -24,4 +24,17 @@ class VkAccount < ActiveRecord::Base
     end
     scope
   end
+
+  def self.update_sex
+    hash = self.group_by(&:vk_id)
+    vk_ids = hash.keys
+    vk_ids.each_slice(1000) do |ids_list|
+      Oj.load(Typhoeus.post("https://api.vk.com/method/getProfiles?fields=city,sex&access_token=794e9fcb0d26fe28c2010a8b87a802c3b82304fd644154d8daf0f676f7662291a3f30835259b81ced0e67", body:{uids: ids_list.join(","), "Content-Type" => 'application/x-www-form-urlencoded'}).body)["response"].each do |account|
+        a = hash[account["uid"].to_i].first
+        a.vk_city_id = account["city"]
+        a.sex = account["sex"]
+        a.save(validate:false)
+      end
+    end
+  end
 end
