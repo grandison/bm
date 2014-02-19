28a525e7187c048fa37f681d330a6b543667b10b
Alexander Osmanov <grandison@mail.ru>
2014-02-19 19:21:46 +0400

    Generate

--- app/models/vk_account.rb
+++ app/models/vk_account.rb
@@ -25,8 +25,7 @@ class VkAccount < ActiveRecord::Base
     scope
   end
 
-  def self.update_sex
-    vk_ids = self.readonly(false).pluck(:vk_id)
+  def self.update_sex(vk_ids)
     vk_ids.each_slice(100) do |ids_list|
       Oj.load(Typhoeus.post("https://api.vk.com/method/getProfiles?fields=city,sex&access_token=794e9fcb0d26fe28c2010a8b87a802c3b82304fd644154d8daf0f676f7662291a3f30835259b81ced0e67", body:{uids: ids_list.join(","), "Content-Type" => 'application/x-www-form-urlencoded'}).body)["response"].each do |account|
         a = VkAccount.find_by_vk_id(account["uid"])
