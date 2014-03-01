256e0300ad7b50388a54942414af2fae8c2c09b3
Alexander Osmanov <grandison@mail.ru>
2014-03-01 13:30:36 +0400

    Fix names

--- app/models/vk_city.rb
+++ app/models/vk_city.rb
@@ -8,14 +8,14 @@ class VkCity < ActiveRecord::Base
   def self.pretty_country(country)
     @countries ||= {}
     @countries[country] ||= begin
-      Oj.load(Typhoeus.post("https://api.vk.com/method/database.getCountriesById?country_ids=#{country}&access_token=794e9fcb0d26fe28c2010a8b87a802c3b82304fd644154d8daf0f676f7662291a3f30835259b81ced0e67", "Content-Type" => 'application/x-www-form-urlencoded'}).body)["response"].first["title"]
+      Oj.load(Typhoeus.get("https://api.vk.com/method/database.getCountriesById?country_ids=#{country}&access_token=794e9fcb0d26fe28c2010a8b87a802c3b82304fd644154d8daf0f676f7662291a3f30835259b81ced0e67").body)["response"].first["name"]
     end
   end
 
   def self.pretty_city(city)
     @countries ||= {}
-    @countries[country] ||= begin
-      Oj.load(Typhoeus.post("https://api.vk.com/method/database.getCitiesById?city_ids=#{city}&access_token=794e9fcb0d26fe28c2010a8b87a802c3b82304fd644154d8daf0f676f7662291a3f30835259b81ced0e67", "Content-Type" => 'application/x-www-form-urlencoded'}).body)["response"].first["title"]
+    @countries[city] ||= begin
+      Oj.load(Typhoeus.get("https://api.vk.com/method/database.getCitiesById?city_ids=#{city}&access_token=794e9fcb0d26fe28c2010a8b87a802c3b82304fd644154d8daf0f676f7662291a3f30835259b81ced0e67").body)["response"].first["name"]
     end
   end
 end
