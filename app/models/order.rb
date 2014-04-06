4c73f470746824e219be40bf8347acb8242a8879
Alexander Osmanov <grandison@mail.ru>
2014-04-06 11:36:51 +0400

    Change token

--- app/models/order.rb
+++ app/models/order.rb
@@ -36,7 +36,7 @@ class Order < ActiveRecord::Base
   def generate_with_names!
     f = File.open(Rails.root.join("public", download_code + ".txt"), "w:windows-1251")
     VkAccount.search(self).each_slice(200) do |accounts|
-      Oj.load(Typhoeus.post("https://api.vk.com/method/getProfiles?fields=city,sex&access_token=f0a3d9796485e7f5cd53cb31fcfb2e963b176de63e783abf9ae330999c1abacd5d35f63b2ac5da27aea6b", body:{uids: accounts.map(&:vk_id).join(","), "Content-Type" => 'application/x-www-form-urlencoded'}).body)["response"].each_with_index do |account,index|
+      Oj.load(Typhoeus.post("https://api.vk.com/method/getProfiles?fields=city,sex&access_token=b7b1326dae84a82bc0fe2b188b41a370e219e5f6d645af9e3921edb5ccbbb645d4d220b74b578403be39e", body:{uids: accounts.map(&:vk_id).join(","), "Content-Type" => 'application/x-www-form-urlencoded'}).body)["response"].each_with_index do |account,index|
         f.write("#{account["first_name"].to_s.encode('windows-1251', {:invalid => :replace, :undef => :replace, :replace => '?'})} #{account["last_name"].to_s.encode('windows-1251', {:invalid => :replace, :undef => :replace, :replace => '?'})},#{accounts[index].email}\n")
       end
       sleep(0.2)
@@ -48,7 +48,7 @@ class Order < ActiveRecord::Base
     f = File.open(Rails.root.join("public", download_code + ".json"), "w:windows-1251")
     f.write("[")
     VkAccount.search(self).each_slice(200) do |accounts|
-      accounts_data = Oj.load(Typhoeus.post("https://api.vk.com/method/getProfiles?fields=photo,sex,bdate,city,country,site,education,universities,schools,status,relation&access_token=f0a3d9796485e7f5cd53cb31fcfb2e963b176de63e783abf9ae330999c1abacd5d35f63b2ac5da27aea6b", body:{uids: accounts.map(&:vk_id).join(","), "Content-Type" => 'application/x-www-form-urlencoded'}).body)["response"]
+      accounts_data = Oj.load(Typhoeus.post("https://api.vk.com/method/getProfiles?fields=photo,sex,bdate,city,country,site,education,universities,schools,status,relation&access_token=b7b1326dae84a82bc0fe2b188b41a370e219e5f6d645af9e3921edb5ccbbb645d4d220b74b578403be39e", body:{uids: accounts.map(&:vk_id).join(","), "Content-Type" => 'application/x-www-form-urlencoded'}).body)["response"]
       cities = []
       accounts_data.each do |account|
         cities << account["city"]
