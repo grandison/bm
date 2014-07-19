3a865e4183a50b0a4f6c378fb82d08a58884a2e0
Alexander Osmanov <grandison@mail.ru>
2014-07-19 20:48:38 +0400

    Names

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ VkMails::Application.routes.draw do
   resources :orders, only: [:new, :create, :show] do
     get :info
     get ':saller_code', to: 'orders#boom'
-    # get :boom_with_names
+    get :boom_with_names
     # get :generate_full
   end
   root to: "orders#new"
